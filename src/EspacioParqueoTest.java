7b3e82de97d27904169145e13e424933d853dac4
juan mosquera <112137410+juandavid-progxx@users.noreply.github.com>
2022-09-27 20:16:55 -0500

    falta unitest

---
+++ src/EspacioParqueoTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EspacioParqueoTest {
+
+    @org.junit.jupiter.api.Test
+    @Test
+    public void getID() {
+
+
+    }
+}
